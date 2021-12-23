c3c296201f9302b1071f3a44fdad45fe3efcfa7e
sangho lee <sangholee.dev@gmail.com>
2021-12-23 22:47:10 +0900

    test: MemberService add member 테스트 추가

--- src/test/kotlin/com/github/leesangho/dev/webfluxstudy/member/application/service/MemberServiceTest.kt
+++ src/test/kotlin/com/github/leesangho/dev/webfluxstudy/member/application/service/MemberServiceTest.kt
@@ -2,6 +2,7 @@ package com.github.leesangho.dev.webfluxstudy.member.application.service
 
 import com.github.leesangho.dev.webfluxstudy.member.adaptor.out.persistence.MemberEntity
 import com.github.leesangho.dev.webfluxstudy.member.adaptor.out.persistence.MemberR2dbcRepository
+import com.github.leesangho.dev.webfluxstudy.member.application.port.`in`.MemberCreateCommand
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
+import org.mockito.Mockito.any
 import org.mockito.junit.jupiter.MockitoExtension
 import reactor.core.publisher.Mono
 
@@ -28,20 +30,36 @@ internal class MemberServiceTest {
     @Test
     @DisplayName("사용자 조회")
     fun getMember() {
+        // given
         val id = 1L
         val name = "test"
         `when`(memberR2dbcRepository.findById(id))
             .thenReturn(Mono.just(MemberEntity(id, name)))
 
+        // when
         val member = memberService.getMember(id)
             .blockOptional().get()
 
+        // then
         assertThat(member.id).isEqualTo(id)
         assertThat(member.name).isEqualTo(name)
     }
 
     @Test
+    @DisplayName("사용자 추가")
     fun addMember() {
+        // given
+        val name = "test"
+        `when`(memberR2dbcRepository.save(any()))
+            .thenReturn(Mono.just(MemberEntity(1, name)))
+
+        // when
+        val member = memberService.addMember(MemberCreateCommand(name))
+            .blockOptional().get()
+
+        // then
+        assertThat(member.id).isNotNull()
+        assertThat(member.name).isEqualTo(name)
     }
 
     @Test
