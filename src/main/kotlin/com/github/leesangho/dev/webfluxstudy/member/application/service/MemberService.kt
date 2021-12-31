3f32212779875b51b7096ebd2f9a24223f04765c
sangho lee <sangholee.dev@gmail.com>
2021-12-31 23:36:48 +0900

    feat: getMembers 추가

--- src/main/kotlin/com/github/leesangho/dev/webfluxstudy/member/application/service/MemberService.kt
+++ src/main/kotlin/com/github/leesangho/dev/webfluxstudy/member/application/service/MemberService.kt
@@ -4,11 +4,19 @@ import com.github.leesangho.dev.webfluxstudy.member.adaptor.out.persistence.Memb
 import com.github.leesangho.dev.webfluxstudy.member.adaptor.out.persistence.MemberR2dbcRepository
 import com.github.leesangho.dev.webfluxstudy.member.application.port.`in`.MemberCreateCommand
 import com.github.leesangho.dev.webfluxstudy.member.domain.Member
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Service
 class MemberService(val memberR2dbcRepository: MemberR2dbcRepository) {
+    fun getMembers(pageable: Pageable, sort: Sort): Flux<Member> {
+        return memberR2dbcRepository.findAll(pageable, sort)
+            .map(MemberEntity::fromThis)
+    }
+
     fun getMember(id: Long): Mono<Member> {
         return memberR2dbcRepository.findById(id)
             .map(MemberEntity::fromThis)
