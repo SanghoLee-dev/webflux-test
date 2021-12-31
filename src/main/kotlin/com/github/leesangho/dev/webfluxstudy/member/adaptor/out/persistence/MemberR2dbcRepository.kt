3bb738fbfced7b6d3ad48b8953f9bde296df2084
sangho lee <sangholee.dev@gmail.com>
2021-12-31 23:36:11 +0900

    refactor: MemberR2dbcRepository ReactiveSortingRepository 로 변경

--- src/main/kotlin/com/github/leesangho/dev/webfluxstudy/member/adaptor/out/persistence/MemberR2dbcRepository.kt
+++ src/main/kotlin/com/github/leesangho/dev/webfluxstudy/member/adaptor/out/persistence/MemberR2dbcRepository.kt
@@ -1,5 +1,10 @@
 package com.github.leesangho.dev.webfluxstudy.member.adaptor.out.persistence
 
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.repository.reactive.ReactiveSortingRepository
+import reactor.core.publisher.Flux
 
-interface MemberR2dbcRepository : ReactiveCrudRepository<MemberEntity, Long>
+interface MemberR2dbcRepository : ReactiveSortingRepository<MemberEntity, Long> {
+    fun findAll(pageable: Pageable, sort: Sort): Flux<MemberEntity>
+}
